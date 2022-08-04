c37038ef2bf8487ad78f48935a65465c26650ee4
leticiacsal <leticia4821@gmail.com>
2022-08-03 21:19:53 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import static java.lang.System.out;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        out.println();
+
+
+     int a= 5;
+     int b= 3;
+     System.out.println("Hello World!" + (a+b));
+    }
+}
+
